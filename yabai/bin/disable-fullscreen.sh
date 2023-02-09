b4aa0ab84af54ee033f54c7b86cacbd2b779cc2a
tokibi <tatu014@gmail.com>
2023-02-09 21:31:36 +0900

    update yabai,skhd config

--- yabai/bin/disable-fullscreen.sh
+++ yabai/bin/disable-fullscreen.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-active_space=$(yabai -m query --spaces --space | jq .index)
-fullscreened_window_id=$(yabai -m query --windows --display | jq '.[] | select(.space == '$active_space' and .["has-fullscreen-zoom"] == true) | .id')
+# Retrieve full-screened window in current space.
+window_id=$(yabai -m query --windows --space | jq '.[] | select(.["has-fullscreen-zoom"] == true) | .id')
 
-if [ -n "$fullscreened_window_id" ]; then
-    yabai -m window --toggle zoom-fullscreen
+if [ -n "$window_id" ]; then
+    yabai -m window $window_id --toggle zoom-fullscreen
 fi
